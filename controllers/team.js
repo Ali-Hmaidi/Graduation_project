f23de90e170b85ea93d63385b28ee5ee1201bcb4
Ali-Hmaidi <ali.hmaidi.h@gmail.com>
2023-01-17 19:55:48 +0200

    modified:   controllers/team.js

--- controllers/team.js
+++ controllers/team.js
@@ -25,11 +25,11 @@ const getTeams = async (req, res) => {
     result = result.select(fieldsList);
   }
 
-  const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit) || 10;
-  const skip = (page - 1) * limit;
+  // const page = Number(req.query.page) || 1;
+  // const limit = Number(req.query.limit) || 10;
+  // const skip = (page - 1) * limit;
 
-  result = result.skip(skip).limit(limit);
+  // result = result.skip(skip).limit(limit);
 
   const teams = await result;
 
