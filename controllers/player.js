3d8af32121a0a4d0f4a12dd52df8cffadbe9f279
Ali-Hmaidi <ali.hmaidi.h@gmail.com>
2023-01-19 23:15:56 +0200

    modified:   controllers/player.js

--- controllers/player.js
+++ controllers/player.js
@@ -4,7 +4,18 @@ const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, NotFoundError } = require("../errors");
 
 const getPlayers = async (req, res) => {
-  const players = await Player.find({});
+  const queryObject = {};
+
+  let result = Player.find(queryObject);
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
+  const players = await result;
+
   res.status(StatusCodes.OK).json({ playersCount: players.length, players });
 };
 
